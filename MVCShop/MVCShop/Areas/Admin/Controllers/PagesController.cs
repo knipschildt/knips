574a6979df8e8141e60c38cb811720698f370a8d
Mikael Knipschildt <knipschildt@live.dk>
2017-10-21 23:01:22 +0200

    lektion 16

--- MVCShop/MVCShop/Areas/Admin/Controllers/PagesController.cs
+++ MVCShop/MVCShop/Areas/Admin/Controllers/PagesController.cs
@@ -163,5 +163,70 @@ namespace MVCShop.Areas.Admin.Controllers
             }
             return View(model);
         }
+
+        public ActionResult DeletePage(int id)
+        {
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+
+                db.Pages.Remove(dto);
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public void ReorderPages(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                int count = 1;
+
+                PageDTO dto;
+
+                foreach (var pageId in id)
+                {
+                    dto = db.Pages.Find(pageId);
+                    dto.Sorting = count;
+                    db.SaveChanges();
+
+                    count++;
+                }
+
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult EditSidebar()
+        {
+            SidebarVM model;
+
+            using (Db db = new Db())
+            {
+                SidebarDTO dto = db.Sidebar.Find(1);
+                model = new SidebarVM(dto);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditSidebar(SidebarVM model)
+        {
+            using(Db db = new Db())
+            {
+                SidebarDTO dto = db.Sidebar.Find(1);
+
+                dto.Body = model.Body;
+
+                db.SaveChanges();
+
+            }
+
+            TempData["SM"] = "You have edited the sidebar!";
+            return RedirectToAction("EditSidebar");
+        }
     }
 }
