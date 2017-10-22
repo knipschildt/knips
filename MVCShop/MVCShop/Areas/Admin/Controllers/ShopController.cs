ff1f005039afcc6fcb723856c96911154915ef77
Mikael Knipschildt <knipschildt@live.dk>
2017-10-22 14:33:27 +0200

    lektion 20

--- MVCShop/MVCShop/Areas/Admin/Controllers/ShopController.cs
+++ MVCShop/MVCShop/Areas/Admin/Controllers/ShopController.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using MVCShop.Models.Data;
+using MVCShop.Models.ViewModels.Shop;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVCShop.Areas.Admin.Controllers
@@ -9,9 +9,20 @@ namespace MVCShop.Areas.Admin.Controllers
     public class ShopController : Controller
     {
         // GET: Admin/Shop
-        public ActionResult Index()
+        public ActionResult Categories()
         {
-            return View();
+            List<CategoryVM> categoryVMList;
+
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories
+                                    .ToArray()
+                                    .OrderBy(x => x.Sorting)
+                                    .Select(x => new CategoryVM(x))
+                                    .ToList();
+            }
+
+            return View(categoryVMList);
         }
     }
 }
